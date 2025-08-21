533c51c025d3faf2f645c687a9f2a117822fb4ca
Saidu O Kamara <kamarasaidu558@gmail.com>
2025-08-21 01:09:50 +0000

    first react-project commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ const name = "Saidu"
+
+  return (
+    <>
+
+      <h1>Hello {name} </h1>
+
+
+    </>
+  )
+}
+
+export default App
